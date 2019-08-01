54db4f51008e0c1550625c0cedff045366921fcf
xueyan <348352046@qq.com>
2019-08-02 00:09:11 +0800

    :pencil: 完成代码。使用 posenet 模型

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,60 +1,86 @@
+const posenet = require('@tensorflow-models/posenet')
+const rr = require('regenerator-runtime')
+const tf = require('@tensorflow/tfjs-core')
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-    
   },
-
-  onLoad: function (options) {
+  async onReady() {
     const camera = wx.createCameraContext(this)
+    this.canvas = wx.createCanvasContext("pose", this)
+    this.loadPosenet()
     let count = 0
-    const lister =  camera.onCameraFrame((frame) => {
+    const lister = camera.onCameraFrame((frame) => {
       count++
-      if(count === 4){
-        console.log(frame)
-        count = 0 
+      if (count === 10) {
+        if(this.net){
+         this.drawImg(frame)
+        }
+        count = 0
         return
       }
     })
-    // lister.start()
-    this.canvas = wx.createCanvasContext("pose", this)
+    lister.start()
+ 
   },
-  onReady: function () {
-    
+  //处理数据
+  async deleFrame(frame,net){
+    const imgData = {data: new Uint8Array(frame.data),width:frame.width,height:frame.height} // 获取图片的数据
+    const imgSlice = tf.tidy(() => { // 此方法用于释放内存
+      const imgTensor = tf.browser.fromPixels(imgData, 3) 
+      return imgTensor.slice([0, 0, 0], [-1, -1, 3])//截取数据
+    })
+    const pose = await net.estimateSinglePose(imgSlice, {
+      flipHorizontal: false // 不反转图片
+    });
+    imgSlice.dispose()
+    return pose
   },
-
-  onShow: function () {
-    
+  //异步加载模型数据
+  async loadPosenet() {
+    this.net = await posenet.load({
+      architecture: 'MobileNetV1',
+      outputStride: 16,
+      inputResolution: 193,
+      multiplier: 0.5
+    })
   },
-
-  onHide: function () {
+  //画canvas
+  async drawImg(frame){
+    const pose = await this.deleFrame(frame, this.net)
+    console.log(pose)
     
+    if(pose == null || this.canvas == null ) return
+  
+    if(pose.score>=0.3 ){ // 姿势打分的数据
+      for(item in pose.keypoints){
+        const point = pose.keypoints[item]
+        if (point.score >= 0.5){
+          const {x,y} = point.position
+          this.drawCircle(this.canvas,x,y)
+        }
+      }
+      // 画线
+      const adjacentKeyPoints = posenet.getAdjacentKeyPoints(pose.keypoints,0.5)
+      for (i in adjacentKeyPoints){
+        const points = adjacentKeyPoints[i]
+        this.drawLine(this.canvas,points[0], points[1])
+      }
+      this.canvas.draw()
+    }
   },
-
-  onUnload: function () {
-    
+  drawCircle(canvas,x,y){
+    canvas.beginPath()
+    canvas.arc(x*0.72,y*0.72,3,0,2*Math.PI)
+    canvas.fillStyle = 'aqua'
+    canvas.fill()
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
+  drawLine(canvas,pos0,pos1){
+    canvas.beginPath()
+    canvas.moveTo(pos0.x*0.72,pos1*0.72)
+    canvas.lineTo(pos0.x*0.72,pos1*0.72)
+    canvas.lineWidth = 2
+    canvas.strokeStyle = 'aqua'
+    canvas.stroke()
   }
 })
